65e72d203c40fdb32eca361e8a53de4d4f619a47
Douglas <kaloblian@gmail.com>
2026-03-06 22:13:04 -0300

    learn(ap-04):props

---
+++ ap04-react-props/src/Feedback.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Feedback = props => {
+  return (
+    <div className='d-flex justify-content-evenly m-3'>
+    <button 
+        type="button" 
+        className="btn btn-primary">
+    </button>
+    <button 
+        type="button" 
+        className="btn btn-danger">
+
+        </button>
+    </div>
+  )
+}
+
+export default Feedback
